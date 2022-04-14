48bdfe00b953072cf74162fb3c55993b086350a8
mkalansari7 <mk.alansari7@gmail.com>
2022-04-14 07:32:23 +0300

    Done

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,9 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    opening_time = models.TextField()
+    closing_time = models.TextField()
